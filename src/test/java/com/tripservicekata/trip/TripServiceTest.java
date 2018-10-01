f2113f3f0cbeb66e075718a9dd3b6a6f97f50e28
cbouttaz <c.bouttaz@gmail.com>
2018-10-01 16:38:16 +0200

    add Trip service Kata

---
+++ src/test/java/com/tripservicekata/trip/TripServiceTest.java
@@ -0,0 +1,12 @@
+package com.tripservicekata.trip;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TripServiceTest {
+
+    @Test
+    public void getTripsByUser_shouldDoSomething_WhenSomeState() {
+        Assert.assertTrue(true);
+    }
+}
